950769d59406e83cee850fdacf4e8e3990a0e7e5
Fingercomp <fingercomp@users.noreply.github.com>
2017-06-02 22:35:01 +0700

    Dumb sprites

--- src/sprite.lua
+++ src/sprite.lua
@@ -1,19 +1,47 @@
 local vector = require("vector")
 
+local module = require("ethel.module")
+local tile = module.load("tile")
+
 local newSprite do
   local meta = {}
 
-  function newSprite(w, h, texture)
+  function newSprite(w, h, isEnemy, render, update, properties)
     return function(x, y)
       local o = {
         x = x,
         y = y,
-        w = w,
+        w = w * 2,
         h = h,
-        texture = texture,
+        emeny = isEnemy,
+        render = render,
+        update = update,
         velocity = vector(0, 0)
       }
+      if properties then
+        properties(o)
+      end
       return setmetatable(o, meta)
     end
   end
 end
+
+local player = newSprite(2, 5, false,
+                         tile.renderFromResource(getResource("sprite.player")),
+                         function() end)
+
+local chort = newSprite(
+  3,
+  3,
+  true,
+  tile.renderFromResource(getResource("sprite.chort")),
+  function(self, window, tilemap)
+    self.velocity = vector(0.05 * self.props.direction, 0)
+  end,
+  function(o)
+    o.direction = 1
+  end)
+
+return {
+  newSprite = newSprite
+}
